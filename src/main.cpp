b68e9464992803e067c4a0a6421f01528597bf3a
Marek-mba <bereza@gmail.com>
2024-05-28 16:07:30 +0100

    most events now work

--- src/main.cpp
+++ src/main.cpp
@@ -12,9 +12,79 @@ public:
 		dylib.recompiled = [&](void *dlib) { liveCodeApp = static_cast<App *>(dlib); };
 		dylib.setup("/Users/marek/mzgl-livecode/src/MyApp.h");
 	}
-	void update() override { dylib.update(); }
+
+	void update() override {
+		dylib.update();
+		if (liveCodeApp) liveCodeApp->update();
+	}
 	void draw() override {
 		if (liveCodeApp) liveCodeApp->draw();
+	}
+
+	bool openUrl(ScopedUrlRef url) override {
+		if (liveCodeApp) return liveCodeApp->openUrl(url);
+		return false;
+	}
+
+	// FILE DROP
+	void fileDragUpdate(float x, float y, int touchId, int numFiles) override {
+		if (liveCodeApp) liveCodeApp->fileDragUpdate(x, y, touchId, numFiles);
+	}
+
+	// return true to accept, false to reject
+	void filesDropped(const std::vector<ScopedUrlRef> &paths, int touchId) override {
+		if (liveCodeApp) liveCodeApp->filesDropped(paths, touchId);
+	}
+
+	void fileDragCancelled(int touchId) override {
+		if (liveCodeApp) liveCodeApp->fileDragCancelled(touchId);
+	}
+	void fileDragExited(float x, float y, int id) override {
+		if (liveCodeApp) liveCodeApp->fileDragExited(x, y, id);
+	}
+
+	// return true if you can open, false if you can't
+	bool canOpenFiles(const std::vector<std::string> &paths) override {
+		if (liveCodeApp) return liveCodeApp->canOpenFiles(paths);
+		return false;
+	}
+
+	void drawAfterUI() override {
+		if (liveCodeApp) liveCodeApp->drawAfterUI();
+	}
+	void exit() override {
+		if (liveCodeApp) liveCodeApp->exit();
+	}
+
+	void resized() override {
+		if (liveCodeApp) liveCodeApp->resized();
+	}
+
+	void mouseScrolled(float x, float y, float dX, float dY) override {
+		if (liveCodeApp) liveCodeApp->mouseScrolled(x, y, dX, dY);
+	}
+	void mouseZoomed(float x, float y, float zoom) override {
+		if (liveCodeApp) liveCodeApp->mouseZoomed(x, y, zoom);
+	}
+
+	void touchOver(float x, float y) override {
+		if (liveCodeApp) liveCodeApp->touchOver(x, y);
+	}
+	void touchDown(float x, float y, int id) override {
+		if (liveCodeApp) liveCodeApp->touchDown(x, y, id);
+	}
+	void touchMoved(float x, float y, int id) override {
+		if (liveCodeApp) liveCodeApp->touchMoved(x, y, id);
+	}
+	void touchUp(float x, float y, int id) override {
+		if (liveCodeApp) liveCodeApp->touchUp(x, y, id);
+	}
+
+	void keyDown(int key) override {
+		if (liveCodeApp) liveCodeApp->keyDown(key);
+	}
+	void keyUp(int key) override {
+		if (liveCodeApp) liveCodeApp->keyUp(key);
 	}
 };
 
