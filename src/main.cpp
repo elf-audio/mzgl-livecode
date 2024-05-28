c109c58f59da323ddcb353d9ac0b6d8996e4da85
Marek-mba <bereza@gmail.com>
2024-05-28 16:00:49 +0100

    now working

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,43 @@
 
 #include "App.h"
-#include "myApp.cpp"
+// #include "myApp.cpp"
+#include "RecompilingAppDylib.h"
 class LiveCodeReloadingApp : public App {
 public:
-    std::shared_ptr<App> liveCodeApp;
-    LiveCodeReloadingApp(Graphics &g)
-		: App(g) {
-        liveCodeApp = std::make_shared<LiveCodeApp>(g);
-    }
-
-	void draw() {
-        liveCodeApp->draw();
+	App *liveCodeApp = nullptr;
+	RecompilingAppDylib dylib;
+	LiveCodeReloadingApp(Graphics &g)
+		: App(g)
+		, dylib(g) {
+		dylib.recompiled = [&](void *dlib) { liveCodeApp = static_cast<App *>(dlib); };
+		dylib.setup("/Users/marek/mzgl-livecode/src/MyApp.h");
+	}
+	void update() override { dylib.update(); }
+	void draw() override {
+		if (liveCodeApp) liveCodeApp->draw();
 	}
 };
 
 std::shared_ptr<App> instantiateApp(Graphics &g) {
-	g.width = 512;
+	g.width	 = 512;
 	g.height = 512;
 
 	auto app = std::make_shared<LiveCodeReloadingApp>(g);
 
 	return app;
 }
+#include "PluginEditor.h"
+std::shared_ptr<PluginEditor> instantiatePluginEditor(Graphics &g, std::shared_ptr<Plugin> plugin) {
+	return nullptr;
+}
+
+std::shared_ptr<Plugin> instantiatePlugin() {
+	return nullptr;
+}
+
+#define DR_FLAC_IMPLEMENTATION
+#define DR_WAV_IMPLEMENTATION
+#define DR_MP3_IMPLEMENTATION
+#include "dr_flac.h"
+#include "dr_mp3.h"
+#include "dr_wav.h"
